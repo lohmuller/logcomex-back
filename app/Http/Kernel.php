66c1d3bd5782f1cc509bef7155f87ae48c34f0ac
Ian <ian@outlook.com.br>
2017-10-03 04:04:08 -0300

    + teste cors

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,5 +58,6 @@ class Kernel extends HttpKernel
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'cors' => \App\Http\Middleware\Cors::class,
+        'client_credentials'     => \Laravel\Passport\Http\Middleware\CheckClientCredentials::class,
     ];
 }
