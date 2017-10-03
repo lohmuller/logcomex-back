674b2ee6e5e8e99c3d090233b93f6d1012bbf473
Ian <ian@outlook.com.br>
2017-10-03 03:50:47 -0300

    + cors

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\Route;
 use Laravel\Passport\Passport;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -26,7 +27,11 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
-        Passport::routes();
+
+        Route::group([ 'middleware' => 'cors'], function() {
+            Passport::routes();
+        });
+        //Passport::routes();
 
         //
     }
